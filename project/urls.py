98543ec725863aa209d3db00ba6085f16ca02baa
vincentrm89 <vincentrm89@gmail.com>
2019-10-30 16:40:48 -0400

    Fixed models

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,24 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 from welcome.views import index, health
+
+app_name="CyNest Django"
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
+    url(r'^$', index, name="index"),
     url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
--- welcome/views.py
+++ welcome/views.py
@@ -1,16 +1,22 @@
 import os
+import uuid
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 
 from . import database
-from .models import PageView
+from .models import PageView, Mission
 
 # Create your views here.
 
 def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
+
+    m1 = Mission()
+    m1.save()
+    m1.is_deleted = True
+    m1.save()
 
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
